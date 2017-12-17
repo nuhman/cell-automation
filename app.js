fda42f14b702901a4acc8b45ab18ec216b4d5d38
nuhman <nuhman7@outlook.com>
2017-12-17 10:38:32 +0530

    create one row of cells with rand state

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
-for(let i=0; i< 51; i++){
+function getRandomState(){
+    if(Math.random() < 0.5) return 0;
+    return 1;
+}
+
+for(let i=0; i< 101; i++){
     let div = document.createElement('div');
-    document.getElementById("cells").appendChild(div);
+    div.classList.add(getRandomState() ? 'active' : 'inactive');    
+    document.querySelector(".cells").appendChild(div);    
 }
 
 
